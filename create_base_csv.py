5b2af6fbdf53d3e913316ff50e8f92f499b96fc0
Alex H <alexhuft@gmail.com>
2022-10-06 14:33:23 -0700

    most up to date csv_maker and base.csv

--- create_base_csv.py
+++ create_base_csv.py
@@ -2,12 +2,14 @@
 
 import pandas as pd
 
+
+CSV_NAME: str = "base.csv"
 link = "https://en.wikipedia.org/wiki/List_of_Regular_Show_episodes#Episodes"
 for table in range(3, 11, 1):
     tables = pd.read_html(link,header=0)[table]
     tables["Season"] = table - 2
     tables["US viewers(millions)"] = tables["US viewers(millions)"].map(lambda s: str(s).split("[")[0])
     if table > 3:
-        tables.to_csv("base.csv", sep=',', index=False, mode="a", header=False)
+        tables.to_csv(CSV_NAME, sep=',', index=False, mode="a", header=False)
     else:
-        tables.to_csv("base.csv", sep=',', index=False, mode="w")
+        tables.to_csv(CSV_NAME, sep=',', index=False, mode="w")
